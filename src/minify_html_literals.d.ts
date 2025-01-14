f00a461bb4c2ec5f309a55c755990854dffc9fbe
Cédric Belin <cedric@belin.io>
2025-01-14 23:26:17 +0100

    Rename the `MinifyOptions` type to `PluginOptions`

--- src/minify_html_literals.d.ts
+++ src/minify_html_literals.d.ts
@@ -3,14 +3,15 @@ import {Options} from "minify-html-literals";
 
 /**
  * Creates a plug-in that minifies HTML markup inside template literal strings.
+ * @param options The plug-in options.
  * @returns The newly created plug-in.
  */
-export function minifyHtmlLiterals(options?: MinifyOptions): Plugin;
+export function minifyHtmlLiterals(options?: PluginOptions): Plugin;
 
 /**
  * Defines the options of the {@link minifyHtmlLiterals} plug-in.
  */
-export type MinifyOptions = Options & Partial<{
+export type PluginOptions = Options & Partial<{
 
 	/**
 	 * The pattern indicating which modules to process when the path matches.
